d8f04c8b7c91aa8955dfc2e962cfc2007b2ee98c
adi94958 <117360640+adi94958@users.noreply.github.com>
2023-09-16 22:29:44 +0700

    Add files via upload

---
+++ Pertemuan 4/KelasDua.java
@@ -0,0 +1,11 @@
+public class KelasDua {
+    {
+        System.out.println(5);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(6);
+        KelasSatu satu = new KelasSatu(); //static -- kosong -- noParameter
+        KelasSatu dua = new KelasSatu(10); //kosong -- parameter
+    }
+}
